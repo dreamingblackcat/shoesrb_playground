adfe1b74d007791956c13497cfdabacfc16df80c
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-05-10 15:23:10 +0630

    Add scrollbars to edit boxes

--- converter.rb
+++ converter.rb
@@ -2,16 +2,16 @@
 APP_WIDTH  = 1024
 APP_HEIGHT = 600
 Shoes.app :width => APP_WIDTH, :height => APP_HEIGHT do
-  stack :width => APP_WIDTH, :margin => 6, :height => APP_HEIGHT - 50 do
+  stack :width => APP_WIDTH, :margin => 6, :height => APP_HEIGHT do
     flow do
       @section_width = (APP_WIDTH / 2) - 6
-      stack :width => @section_width do
+      stack :width => @section_width, :margin => 25 do
         para "Unicode"
-        edit_box :width => 200
+        edit_box :width => @section_width - 50, :height => APP_HEIGHT - 72, :scroll => true
       end
-      stack :width => @section_width do
+      stack :width => @section_width, :margin => 25 do
         para "Zawgyi"
-        edit_box :width => 200
+        edit_box :width => @section_width - 50, :height => APP_HEIGHT - 72, :scroll => true
       end
     end
     flow :margin => APP_WIDTH / 3 do
